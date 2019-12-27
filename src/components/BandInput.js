64df751b648b8f75d8fad85cf4610b069f83a144
Justina Kuchta <justyna.kuchta@gmail.com>
2019-12-27 16:08:42 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,34 @@
 // Add BandInput component
 import React, { Component } from 'react'
+// import {connect} from 'react-redux'
 
 class BandInput extends Component {
+
+  state = {name: ''}
+
+  handleChange = e => {
+    this.setState({name: e.target.value})
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.addBand(this.state)
+    this.setState({name: ''})
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input 
+          type='text' 
+          onChange={e => this.handleChange(e)}>
+          </input>
+        </form>
       </div>
     )
   }
 }
 
+// export default connect({addBand})(BandInput)
 export default BandInput
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,11 @@
 export default function manageBand(state = {
-  bands: [],
-}, action) {
-  return state
+  bands: [], }, action) {
+    switch(action.type) {
+      case 'ADD_BAND': return {bands: state.bands.concat(action.band)}
+      // why didn't it work with payload???
+      // case 'ADD_BAND': return {bands: state.bands.concat(action.payload.band)}
+      // alternatively:
+      // case 'ADD_BAND': return { ...state, bands: [...state.bands, action.band] }
+      default: return state
+    }
 };
