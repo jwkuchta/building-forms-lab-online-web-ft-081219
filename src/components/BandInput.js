653b81f8cee939d1d13f71cce2d6ac3ebf1fd460
Justina Kuchta <justyna.kuchta@gmail.com>
2019-12-27 16:22:17 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -14,16 +14,24 @@ class BandInput extends Component {
     e.preventDefault()
     this.props.addBand(this.state)
     this.setState({name: ''})
+    // clear the input area for uncontrolled forms:
+    // this.myFormRef.reset()
   }
 
   render() {
     return(
       <div>
-        <form onSubmit={e => this.handleSubmit(e)}>
+        <form 
+        onSubmit={e => this.handleSubmit(e)}
+        // this is for uncontroller forms only:
+        // ref={(el) => this.myFormRef = el}
+        >
           <input 
           type='text' 
-          onChange={e => this.handleChange(e)}>
-          </input>
+          // you have to put value here or the input field won't clear:
+          value={this.state.name}
+          onChange={e => this.handleChange(e)}/>
+          <input type="submit"/>
         </form>
       </div>
     )
